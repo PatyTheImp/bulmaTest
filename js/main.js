30afa510ef36ee3505e8ef99cd43ab5cb9c77666
Patrícia Costa <pattycosta91@gmail.com>
2022-02-23 21:30:55 +0000

    no bulma

--- js/main.js
+++ js/main.js
@@ -1,37 +1,34 @@
-const filmsSelect = document.querySelector('.films-select');
-const filmsTitle = document.querySelector('.film-title');
-const originalTitle = document.querySelector('.original-title');
-const noFilm = document.querySelector('.no-film');
-const filmInfo = document.querySelector('.film-info');
+const filmsSelect = document.querySelector(".films-select");
+const filmsTitle = document.querySelector(".film-title");
+const originalTitle = document.querySelector(".original-title");
+const noFilm = document.querySelector(".no-film");
+const filmInfo = document.querySelector(".film-info");
 
 //Acrescenta os filmes no dropdown
 async function populateDropdown() {
-
-    try {
-
-        const res = await fetch('https://ghibliapi.herokuapp.com/films');
-        const data = await res.json();
-        data.forEach(d => {
-
-            filmsSelect.insertAdjacentHTML('beforeend', `<option value="${d.id}">${d.title}</option>`);
-        });
-    }
-    catch (error) {
-
-        alert(error.message);
-    }
+  try {
+    const res = await fetch("https://ghibliapi.herokuapp.com/films");
+    const data = await res.json();
+    data.forEach((d) => {
+      filmsSelect.insertAdjacentHTML(
+        "beforeend",
+        `<option value="${d.id}">${d.title}</option>`
+      );
+    });
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
 populateDropdown();
 
 function renderMovieInfo(data) {
+  const html = `
+        <h1 class="film-title">${data.title}</h1>
+        <h1 class="original-title">${data.original_title}</h1>
+        <p>${data.original_title_romanised}</p>
 
-    const html = `
-        <h1 class="title is-2 film-title">${data.title}</h1>
-        <h1 class="title original-title">${data.original_title}</h1>
-        <p class="subtitle">${data.original_title_romanised}</p>
-
-        <p class="has-text-justified"><b>Description: </b>${data.description}</p>
+        <p><b>Description: </b>${data.description}</p>
         <p><b>Director: </b>${data.director}</p>
         <p><b>Producer: </b>${data.producer}</p>
         <p><b>Release Year: </b>${data.release_date}</p>
@@ -55,67 +52,52 @@ function renderMovieInfo(data) {
         </ul>
     `;
 
-    filmInfo.innerHTML = '';
-    filmInfo.insertAdjacentHTML('beforeend', html);
+  filmInfo.innerHTML = "";
+  filmInfo.insertAdjacentHTML("beforeend", html);
 }
 
 async function listInfo(URL, className) {
+  try {
+    const res = await fetch(URL);
+    const data = await res.json();
+    const { name } = data;
+    const listEl = document.querySelector(`.${className}`);
+    let listItem;
 
-    try {
+    if (!name) listItem = "Nothing found";
+    else listItem = name;
 
-        const res = await fetch(URL);
-        const data = await res.json();
-        const { name } = data;
-        const listEl = document.querySelector(`.${className}`);
-        let listItem;
-
-        if (!name)
-            listItem = 'Nothing found';
-        else
-            listItem = name;
-
-        listEl.insertAdjacentHTML('beforeend', `<li>${listItem}</li>`);
-    }
-    catch (error) {
-
-        alert(error.message);
-    }
-
+    listEl.insertAdjacentHTML("beforeend", `<li>${listItem}</li>`);
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
 //Quando se muda a opção no dropdown, mostra a informação do filme seleccionado
-filmsSelect.addEventListener('change', async function () {
+filmsSelect.addEventListener("change", async function () {
+  const id = filmsSelect.value;
 
-    const id = filmsSelect.value;
+  //Se nenhum filme for seleccionado, esconde a informação de filmes
+  if (id == 0) {
+    noFilm.classList.remove("is-hidden");
+    filmInfo.classList.add("is-hidden");
+  } else {
+    try {
+      const res = await fetch(`https://ghibliapi.herokuapp.com/films/${id}`);
+      const data = await res.json();
+      const { people, species, locations, vehicles } = data;
 
-    //Se nenhum filme for seleccionado, esconde a informação de filmes
-    if (id == 0) {
+      renderMovieInfo(data);
 
-        noFilm.classList.remove('is-hidden');
-        filmInfo.classList.add('is-hidden');
+      people.forEach((url) => listInfo(url, "people-list"));
+      species.forEach((url) => listInfo(url, "species-list"));
+      locations.forEach((url) => listInfo(url, "locations-list"));
+      vehicles.forEach((url) => listInfo(url, "vehicles-list"));
+
+      noFilm.classList.add("is-hidden");
+      filmInfo.classList.remove("is-hidden");
+    } catch (error) {
+      alert(error.message);
     }
-    else {
-
-        try {
-
-            const res = await fetch(`https://ghibliapi.herokuapp.com/films/${id}`);
-            const data = await res.json();
-            const { people, species, locations, vehicles } = data;
-
-            renderMovieInfo(data);
-
-            people.forEach(url => listInfo(url, 'people-list'));
-            species.forEach(url => listInfo(url, 'species-list'));
-            locations.forEach(url => listInfo(url, 'locations-list'));
-            vehicles.forEach(url => listInfo(url, 'vehicles-list'));
-
-            noFilm.classList.add('is-hidden');
-            filmInfo.classList.remove('is-hidden');
-        }
-        catch (error) {
-
-            alert(error.message);
-        }
-    }
+  }
 });
-
